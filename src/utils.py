ad02d40a9da172c2d6f5ddcf796984b99763203e
ParagGhatage <phghatage1@gmail.com>
2024-10-04 13:48:30 +0530

    adding utils.py

---
+++ src/utils.py
@@ -0,0 +1,47 @@
+import os
+from tensorflow.keras.models import load_model  
+import tensorflow as tf
+def load_model(model_path,custom_loss=False):
+    """
+    Load a model from the local directory and return the model object.
+
+    Args:
+        model_file_name (str): The name of the model file to load (e.g., 'ResNet50V2.keras').
+        local_model_dir (str): Local directory where the model is stored.
+
+    Returns:
+        model: The loaded model object.
+    
+    Raises:
+        FileNotFoundError: If the model file does not exist at the specified path.
+    """
+    # Create the full local path for the model
+    local_model_path = model_path
+    
+    def dice_loss(y_true, y_pred, smooth=1e-6, weight_bg=0.2, weight_fg=0.8):
+        y_true_f = tf.keras.backend.flatten(y_true)
+        y_pred_f = tf.keras.backend.flatten(y_pred)
+
+        # Compute the intersection
+        intersection = tf.keras.backend.sum(y_true_f * y_pred_f)
+
+        # Compute the Dice coefficient
+        dice = (2. * intersection + smooth) / (tf.keras.backend.sum(y_true_f) + 
+                                               tf.keras.backend.sum(y_pred_f) + smooth)
+
+        return 1 - dice
+    
+    if(custom_loss==True):
+        print(f' {local_model_path}  with custom loss loaded successfully')
+        # For segmentation model, pass the custom_objects parameter
+
+
+        return  load_model(local_model_path, custom_objects={'dice_loss': dice_loss})
+        
+    else:
+        # Check if the model file exists
+        if os.path.exists(local_model_path):
+            print(f' {local_model_path} loaded successfully')
+            return load_model(local_model_path)  # Load and return the model object
+        else:
+            raise FileNotFoundError(f'{local_model_path} not found.')
