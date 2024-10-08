6b4ea4a4c47aa7d1a76d5f544287d892a8ef31cf
ParagGhatage <phghatage1@gmail.com>
2024-10-08 08:44:16 +0530

    Adding Analytics

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <Analytics/>
       <Navbar/>
       
         {children}
